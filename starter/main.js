29579ac61c90c0e72f313d3aa18e7371bdefb884
Adaramola Judah <judahdaramola10@gmail.com>
2022-10-13 13:47:24 +0100

    done a lot of stuffs with little bit of javascript

---
+++ starter/main.js
@@ -0,0 +1,12 @@
+
+// console.log("send me your account number")
+
+// var writeup = "I am hungry"
+
+var hamburgerdiv = document.querySelector(".hamburger")
+
+// console.log(hamburgerdiv)
+
+function dosomething(){
+   hamburgerdiv.classList.toggle("showburger")
+}
