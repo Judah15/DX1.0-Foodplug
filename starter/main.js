19ca50791c4af0f15e55a4de9dad31ac7ebdf2c3
Adaramola Judah <judahdaramola10@gmail.com>
2022-10-14 12:45:28 +0100

    I did lots of stuff today, including the hiding icons

--- starter/main.js
+++ starter/main.js
@@ -4,9 +4,13 @@
 // var writeup = "I am hungry"
 
 var hamburgerdiv = document.querySelector(".hamburger")
+var mobileLinks = document.querySelector(".mobile-link-holder")
+var mybackdrop = document.querySelector(".backdrop")
 
 // console.log(hamburgerdiv)
 
 function dosomething(){
    hamburgerdiv.classList.toggle("showburger")
+   mobileLinks.classList.toggle("show-mobile-link-holder")
+   mybackdrop.classList.toggle("show-backdrop")
 }
